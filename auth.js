36e10a028351fae4d8f4b603414fb0dc9488aad6
Reynaldi Arya <reynaldiarya.id@gmail.com>
2024-07-11 22:01:22 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,14 +5,16 @@ const port = 5000;
 console.log('Ready !');
 const token = Math.floor(100000000000000 + Math.random() * 900000000000000).toString(36);
 console.log('Token: ', token)
-const Authorization = req.headers.authorization;
-if (Authorization == token) {
-    next();
-} else {
-    res.status(401).json({
-        status: false,
-        message: 'Error',
-        meta: 'Not Authorized'
-    })
-}
+app.get('/auth', (req, res) => {
+    const Authorization = req.headers.authorization;
+    if (Authorization == token) {
+        next();
+    } else {
+        res.status(401).json({
+            status: false,
+            message: 'Error',
+            meta: 'Not Authorized'
+        })
+    }
+});
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
