ba15b485be99f0c4d979f8840cdaf4e0bf31a60a
Reynaldi Arya <reynaldiarya.id@gmail.com>
2026-03-27 20:17:28 +0700

    Fix

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ const client = new Client({
   webVersionCache: {
     type: 'remote',
     remotePath:
-      'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+      'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1036094556-alpha.html',
   },
   puppeteer: {
     headless: true,
